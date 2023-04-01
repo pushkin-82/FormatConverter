0a1ad4a35484e74ab3bc99f92efa9b8a87e928ef
Serhii Putylin <serhii.putylin@blockpit.io>
2023-04-01 18:16:32 +0300

    Initial commit

---
+++ src/main/java/com/putylin/formatconverter/FormatConverterApplication.java
@@ -0,0 +1,13 @@
+package com.putylin.formatconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormatConverterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FormatConverterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/putylin/formatconverter/FormatConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.putylin.formatconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormatConverterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
